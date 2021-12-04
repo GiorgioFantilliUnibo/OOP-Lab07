20a6e07f5009c6ddc1c991a6d811ad39f73f3108
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-04 17:42:47 +0100

    feat: implement second sort with Comparator

--- src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
+++ src/it/unibo/oop/lab/anonymous1/TestAnonymousComparator.java
@@ -110,7 +110,12 @@ public final class TestAnonymousComparator {
          * NOTE: in order to sort a list think about a method of the utility
          * class Collections
          */
-        // TODO
+        rossiUsers.sort(new java.util.Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o2.getAge() - o1.getAge();
+            }
+        });
         /*
          * expected Result
          */

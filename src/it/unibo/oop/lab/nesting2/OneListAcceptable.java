1a182195e0e36adf20d1fbccb4102a3165d39068
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-05 19:43:37 +0100

    doc: add javadoc to OneListAcceptable and acceptor()

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,7 +1,18 @@
 package it.unibo.oop.lab.nesting2;
 
+import it.unibo.oop.lab.socialnetwork.User;
+
+/**
+ * Represent a list of {@link Acceptable} elements
+ * 
+ * @param <T>
+ *             specific elements type
+ */
 public class OneListAcceptable<T> implements Acceptable<T> {
 
+    /**
+     *{@inheritDoc}
+     */
     @Override
     public Acceptor<T> acceptor() {
         // TODO Auto-generated method stub

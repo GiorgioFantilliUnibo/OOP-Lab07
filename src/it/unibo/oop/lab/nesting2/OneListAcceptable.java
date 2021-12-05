66c88487c1491836100a6a40bc88862b6209c452
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-05 19:50:31 +0100

    feat: implement constructor

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,5 +1,9 @@
 package it.unibo.oop.lab.nesting2;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * Represent a list of {@link Acceptable} elements.
  * 
@@ -7,8 +11,12 @@ package it.unibo.oop.lab.nesting2;
  *             specific elements type.
  */
 public class OneListAcceptable<T> implements Acceptable<T> {
-    
+
     private final List<T> elements;
+    
+    public OneListAcceptable(final List<T> list) {
+        this.elements = new LinkedList(Objects.requireNonNull(list));
+    }
 
     /**
      *{@inheritDoc}
